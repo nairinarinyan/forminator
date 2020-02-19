754e0d2e9b60387d6742aa5807a75e90896a819e
Nairi Narinyan <nairi.narinyan@gmail.com>
2020-02-19 17:38:59 +0400

    add email validator

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, createContext, FormEvent, useEffect } from 'react';
+import React, { FunctionComponent, createContext, FormEvent } from 'react';
 import { Forminator } from '../forminator';
 import { FieldStates, useForm } from '../use-form';
 
@@ -13,19 +13,12 @@ interface FormContext {
 
 export const FormContext = createContext<FormContext>(null);
 
-// const fieldStatesToValues = (fieldStates: FieldStates) => {
-//     return Object.entries(fieldStates)
-//         .map(([key, [value]]) => ({ [key]: value }))
-//         .reduce((acc, curr) => Object.assign(acc, curr));
-// };
-
 export const Form: FunctionComponent<Props> = props => {
     const { form, children } = props;
     const fieldStates = useForm(form);
 
     const onSubmit = (evt: FormEvent) => {
         evt.preventDefault();
-        // const values = fieldStatesToValues(fieldStates);
         form.submit();
     };
 
--- src/validation.ts
+++ src/validation.ts
@@ -1,40 +1,41 @@
 import {FieldDescriptor, FieldsDescriptors} from "./forminator";
 
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
 export class ValidationError {
     constructor(public message?: string, public errors?: ValidationError[]) {
     }
 }
 
-export type Validator = (field: FieldDescriptor, fields: FieldsDescriptors) => boolean | ValidationError;
+// throw ValidationError if invalid
+export type Validator = (field: FieldDescriptor, fields: FieldsDescriptors) => void;
 
-export const minLength = (message: string, length: number) => (field: FieldDescriptor) => {
+export const minLength = (message: string, length: number): Validator => (field: FieldDescriptor) => {
     if (field.value.length < length) {
         throw new ValidationError(message);
     }
-
-    return true;
 }
 
-export const maxLength = (message: string, length: number) => (field: FieldDescriptor) => {
+export const maxLength = (message: string, length: number): Validator => (field: FieldDescriptor) => {
     if (field.value.length > length) {
         throw new ValidationError(message);
     }
-
-    return true;
 }
 
-export const required = (message: string) => (field: FieldDescriptor) => {
+export const required = (message: string): Validator => (field: FieldDescriptor) => {
     if (field.value === '' || field.value === undefined) {
         throw new ValidationError(message);
     }
-
-    return true;
 };
 
-export const same = (message: string, comparisonField: string) => (field: FieldDescriptor, fields: FieldsDescriptors) => {
+export const same = (message: string, comparisonField: string): Validator => (field, fields) => {
     if (fields[comparisonField].value !== field.value) {
         throw new ValidationError(message);
     }
+};
 
-    return true;
+export const email = (message: string): Validator => field => {
+    if (!emailRegex.test(field.value)) {
+        throw new ValidationError(message);
+    }
 };
