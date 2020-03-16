12366a2cde4578300be5639ecf355e8b6b950b21
Nairi Narinyan <nairi.narinyan@gmail.com>
2020-03-16 13:35:30 +0400

    dont put onsubmit in useform deps

--- src/hooks/use-form.ts
+++ src/hooks/use-form.ts
@@ -4,7 +4,7 @@ import { useMemo } from 'react';
 export const useForm = (descriptor: FormDescriptor, onSubmit: SubmitFn): Forminator => {
     const form = useMemo(() => {
         return new Forminator({ ...descriptor, onSubmit });
-    }, [descriptor, onSubmit]);
+    }, [descriptor]);
 
     return form;
 };
