fceceab91fdc020a9489da5fb5a4b574e5a4f0f4
Nairi Narinyan <nairi.narinyan@gmail.com>
2021-12-15 16:59:19 +0400

    add form props

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,10 +1,10 @@
-import React, { FunctionComponent, createContext, FormEvent } from 'react';
+import React, { FunctionComponent, createContext, FormEvent, HtmlHTMLAttributes, FormHTMLAttributes } from 'react';
 import { Forminator } from '../forminator';
 import { FieldStates, useFieldStates } from '../hooks/use-field-states';
 
-interface Props {
+type Props = FormHTMLAttributes<HTMLFormElement> & {
     form: Forminator<any, any>;
-}
+};
 
 interface FormContext<T extends object, A extends object> {
     fieldStates: FieldStates<T>;
@@ -13,8 +13,7 @@ interface FormContext<T extends object, A extends object> {
 
 export const FormContext = createContext<FormContext<any, any>>(null);
 
-export const Form: FunctionComponent<Props> = props => {
-    const { form, children } = props;
+export const Form: FunctionComponent<Props> = ({ form, children, ...props }) => {
     const fieldStates = useFieldStates(form);
 
     const onSubmit = (evt: FormEvent) => {
@@ -24,7 +23,7 @@ export const Form: FunctionComponent<Props> = props => {
 
     return (
         <FormContext.Provider value={{ fieldStates, form }}>
-            <form onSubmit={onSubmit} id={form.id}>
+            <form onSubmit={onSubmit} id={form.id} {...props}>
                 {children}
             </form>
         </FormContext.Provider>
